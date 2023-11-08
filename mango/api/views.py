bdd1829bee300b724c763575775552fce9f666b6
Filipa Andrade <fandrade@laterpay.net>
2023-11-08 16:03:09 +0100

    Split joke into question and answer

--- mango/api/views.py
+++ mango/api/views.py
@@ -8,4 +8,14 @@ class GetJokeView(View):
         """Get a joke"""
         response = requests.get("https://icanhazdadjoke.com/", headers={"Accept": "application/json"})
         joke = response.json()["joke"]
-        return HttpResponse(joke)
+
+        # Split the joke into question and answer
+        if "?" in joke:
+            split_joke = joke.split("?")
+            question = f"{split_joke[0]} ?"
+            answer = split_joke[1]
+        else:
+            answer = ""
+
+        return HttpResponse(f'<div id="joke-1" class="text-xl font-medium text-black" hx-swap-oob="true">{question}<div>'
+         f'<div id="joke-2" class="text-slate-500" hx-swap-oob="true">{answer}</div>')
