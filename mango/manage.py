06fdb97d4f2a32ee14596111464543ba42fa4418
Filipa Andrade <fandrade@laterpay.net>
2023-11-15 14:11:34 +0000

    Re-add question mark and ruff lint and format

--- mango/manage.py
+++ mango/manage.py
@@ -11,7 +11,7 @@ load_dotenv()
 
 def main():
     """Run administrative tasks."""
-    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'mango.settings')
+    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "mango.settings")
     try:
         from django.core.management import execute_from_command_line
     except ImportError as exc:
@@ -23,5 +23,5 @@ def main():
     execute_from_command_line(sys.argv)
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     main()
--- mango/mango/views.py
+++ mango/mango/views.py
@@ -1,38 +1,36 @@
 import requests
-from django.http import HttpResponse
 from django.views.generic import TemplateView
-from django.conf import settings
+
 
 def get_joke():
-    """Get a joke"""
-    response = requests.get("https://icanhazdadjoke.com/", headers={"Accept": "application/json"})
+    response = requests.get(
+        "https://icanhazdadjoke.com/", headers={"Accept": "application/json"}
+    )
     joke = response.json()["joke"]
 
-    # if the joke do not have a punch line try it again and again
+    # if the joke does not have a punch line try it again and again
     # the punch line is the part after the question mark
     if "?" not in joke:
         return get_joke()
 
     # split the joke into question and answer
     question, answer = joke.split("?")
-    return question, answer
+    return f"{question}?", answer
 
 
 class IndexView(TemplateView):
     """
-    Index view
     Renders the index.html template with a joke context
     """
 
-    """return joke.html if request header contains htmx"""
     def get_template_names(self):
+        # Return joke.html if request header contains htmx
         if self.request.headers.get("Hx-Request"):
             return ["joke.html"]
         else:
             return ["index.html"]
 
     def get_context_data(self, **kwargs):
-        """Get the context"""
         context = super().get_context_data(**kwargs)
         context["question"], context["answer"] = get_joke()
         return context
